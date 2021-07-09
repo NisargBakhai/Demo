7e4dd09ccacd10efd09a8acb151ba6d9007ef154
NisargBakhai <nisargbakhai1999@gmail.com>
2021-07-09 19:01:11 +0530

    initial

---
+++ src/main/java/com/example/demo/web/rest/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/test")
+    ResponseEntity<String> testFunction(){
+        return ResponseEntity.ok().body("Hello from Nisarg!");
+    }
+
+
+}
