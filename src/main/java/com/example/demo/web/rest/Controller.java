8123f980d7ca28509caaf7e24f37c856ca4ccfe8
NisargBakhai <53365397+NisargBakhai@users.noreply.github.com>
2021-07-26 16:41:00 +0530

    Update Controller.java

--- src/main/java/com/example/demo/web/rest/Controller.java
+++ src/main/java/com/example/demo/web/rest/Controller.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class Controller {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     ResponseEntity<String> testFunction(){
         return ResponseEntity.ok().body("Hello from Nisarg again again");
         
